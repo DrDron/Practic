11c0d352e9cddddab270142272b8be7276644e0e
Vitaly Yulis <vitaly.yulis@gmail.com>
2021-03-23 09:48:38 +0300

    Фикс с замечаниями
    - Фикс имени CMakeFiles.txt;
    - Фикс версии cmake (достаточно версии 3.1 вместо 3.19);
    - Нельзя было настроить частоту тона через параметры;
    - Табличная генерация была только для тона 1000 Гц;
    - Таблица синуса хранилась в double векторе, но была приведена к типу short;
    - Генерация сигнала производилась сразу в файл без возможности дозаписи;
    - Частота дискретизации задается только в конструкторе объекта (так проще);
    - Рефакторинг кода.

--- src/main.cpp
+++ src/main.cpp
@@ -5,100 +5,123 @@
 #include <math.h>
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-class Sinusoid {
+class Sinusoid
+{
 private:
-    uint32_t sampleRate;
-    uint16_t amplitude;
-    vector <double> sampleTable;
+    uint32_t m_sampleRate;
+    uint16_t m_amp;
+    int16_t m_freq;
+    int16_t m_phase;                ///< текущая фаза сигнала
+    vector<double> m_sinTable;      ///<  таблица синуса 1Гц для быстрой генерации
+
+    //генерация таблицы синуса (1Гц для данной частоты дискретизации)
+    void GenerateSinTable() {
+        for (int i = 0; i < m_sampleRate; i++) {
+            double t = i / double(m_sampleRate);
+            double value = sin(2 * M_PI * t);
+            m_sinTable.push_back(value);
+        }
+    }
+
 public:
+    Sinusoid(uint32_t sampleRate=8000, uint16_t amp=1000, int16_t freq=440) {
+        m_sampleRate = sampleRate;
+        m_phase = 0;
+        SetAmplitude(amp);
+        SetFreq(freq);
+        GenerateSinTable();
+    };
 
-    void SetSampleRate(uint32_t SR) {
-        sampleRate = SR;
+    ~Sinusoid(){};
+
+    void SetAmplitude(uint16_t amp) {
+        m_amp = amp;
     }
-    unsigned short GetSampleRate() {
-        return sampleRate;
-    };
-    void SetAmplitude(uint16_t Am) {
-        amplitude = Am;
-    }
-    unsigned short GetAmplitude() {
-        return amplitude;
-    };
-    void SetSampleTable() {
-        for (double i = 0; i < sampleRate; i++) {
-            double t = i/ sampleRate;
-            sampleTable.push_back(short(amplitude * sin(2 * M_PI * 1000 * t)));
+
+    void SetFreq(int16_t freq) {
+        if (freq > m_sampleRate/2) {
+            throw std::runtime_error("Freq value error");
         }
+        
+        m_phase = 0;
+        m_freq = freq;
     }
-    void BuildT(FILE* filename) {
-        for (int i = 0; i < sampleRate; i++) {
-            uint8_t c;
-            c = (unsigned)sampleTable[i] % 256;
-            fwrite(&c, 1, 1, filename);
-            c = (unsigned)sampleTable[i] / 256 % 256;
-            fwrite(&c, 1, 1, filename);
+
+    //генерация фрейма длиной nx отсчетов
+    std::vector<int16_t> Generate(int nx) {
+        std::vector<int16_t> r(nx);
+        for (size_t i = 0; i < nx; i++) {
+            r[i] = round(m_amp * m_sinTable[m_phase]);
+            m_phase = (m_phase + m_freq) % m_sampleRate;
         }
-
+        return r;
     }
-
-    Sinusoid(uint32_t SR, uint16_t Am)
-    {
-        SetSampleRate(SR);
-        SetAmplitude(Am);
-        SetSampleTable();
-    };
-    
-    void BuildSinusoid(int drt, const char* filePath) {
-        FILE* pcmFile = fopen(filePath, "wb");
-        for (int i = 0; i < drt; i++) {
-            BuildT(pcmFile);
-        };
-        fclose(pcmFile);
-    };
-    ~Sinusoid() {};
-
-
 };
 
-int main(int argc, char* argv[])
-{
-    argparse::ArgumentParser program("sinus_builder");
+//дописать сигнал в файл
+static void AddToFile(const std::vector<int16_t>& frame, FILE* file) {
+    for (size_t i = 0; i < frame.size(); i++) {
+        int16_t value = frame[i];
+        uint8_t c = value & 0xFF;
+        fwrite(&c, 1, 1, file);
+        c = (value >> 8) & 0xFF;
+        fwrite(&c, 1, 1, file);
+    }
+}
 
-    program.add_argument("-f","--flnm")
+int main(int argc, char *argv[])
+{
+    argparse::ArgumentParser parser("sinus_builder");
+
+    //TODO: использовать более понятные сокращения
+    parser.add_argument("-f", "--flnm")
         .help("name of file with sin")
         .default_value(string("test.pcm"));
-    program.add_argument("-s","--smplrt")
+    parser.add_argument("-s", "--smplrt")
         .required()
         .help("samplerate of sin")
-        .action([](const string& value) { return stoi(value); });
-    program.add_argument("-a","--amp")
+        .action([](const string &value) { return stoi(value); });
+    parser.add_argument("-q", "--freq")
+        .required()
+        .help("frequency of sin")
+        .action([](const string &value) { return stoi(value); });
+    parser.add_argument("-a", "--amp")
         .required()
         .help("amplitude of sin")
-        .action([](const string& value) { return stoi(value); });
-    program.add_argument("-d","--drt")
+        .action([](const string &value) { return stoi(value); });
+    parser.add_argument("-d", "--drt")
         .required()
         .help("duration of pcm")
-        .action([](const string& value) { return stoi(value); });
+        .action([](const string &value) { return stoi(value); });
+
     try {
-        program.parse_args(argc, argv);   // Example: ./main --flnm test.pcm --smplrt 44100 --amp 15000 --drt 4
+        parser.parse_args(argc, argv); // Example: ./main --flnm test.pcm --smplrt 44100 --amp 15000 --drt 4
     }
-    catch (const std::runtime_error& err) {
+    catch (const std::runtime_error &err) {
         cout << err.what() << endl;
-        cout << program;
+        cout << parser;
         exit(0);
     }
 
-    auto inputSR = program.get<int>("-s");
-    auto inputA = program.get<int>("-a");
-    auto inputD = program.get<int>("-d");
-    string inputFN = program.get<string>("-f");
+    auto sampleRate = parser.get<int>("-s");
+    auto amplutude = parser.get<int>("-a");
+    auto duration = parser.get<int>("-d");
+    auto freq = parser.get<int>("-q");
+    string fname = parser.get<string>("-f");
 
-    const char* file = inputFN.c_str();
+    Sinusoid sin(sampleRate, amplutude, freq);
+    FILE* pcmFile = fopen(fname.c_str(), "wb");
 
-    Sinusoid sin(inputSR, inputA);
-    sin.BuildSinusoid(inputD, file);
-	
+    //генерация duration сек (без разрыва фазы)
+    for (size_t i = 0; i < duration; i++) {
+        auto frame = sin.Generate(sampleRate);
+        AddToFile(frame, pcmFile);
+    }
+
+    fclose(pcmFile);
+    
     return 0;
 }
